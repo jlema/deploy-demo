9d1e632f9dc72fa885803594d724a291892e51a5
John (Juan) Lema <jlema@users.noreply.github.com>
2020-06-17 20:26:05 -0700

    VSchool Deploying MERN with Heroku example

--- server.js
+++ server.js
@@ -13,13 +13,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// serve static files of React app from client/build folder
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use("/tweets", tweetRoute);
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/twitter", { useMongoClient: true }, (err) => {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/twitter", { useMongoClient: true }, (err) => {
     if (err) console.error(err);
 });
 
+// serve index.html on GET requests
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(port, () => {
     console.log("Listening on " + port);
 });
